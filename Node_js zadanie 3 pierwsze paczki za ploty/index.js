e93cb04ecbb3d3e53e787890f320953e0025182f
AdamReszka <adam.reszka85@gmail.com>
2017-11-01 18:38:42 +0100

    add third task for kodilla training

---
+++ Node_js zadanie 3 pierwsze paczki za ploty/index.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function(){
+  var OSinfo = require('./modules/OSinfo');
+  var input = process.stdin.read();
+  if(input !== null){
+    var instruction = input.toString().trim();
+    switch(instruction){
+      case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      case '/sayhello':
+        process.stdout.write('hello, hi my friend!\n');
+        break;
+      case '/getOSinfo':
+        OSinfo.print();
+        break;
+      default:
+        process.stderr.write('This is wrong instruction! Try again!\n');
+    }
+  }
+});
---
+++ Node_js zadanie 3 pierwsze paczki za ploty/modules/timeCalc.js
@@ -0,0 +1,27 @@
+var os = require('os');
+var colors = require('colors');
+
+function timeCalc(){
+    var myTime = os.uptime();
+    var myHours = Math.floor(myTime/3600);
+    var secondsLeft = (myTime - myHours * 3600);
+    var myMinutes = Math.floor(secondsLeft/60);
+    var mySeconds = Math.floor(secondsLeft - myMinutes * 60);
+    process.stdout.write('System jest aktywny ')
+    if (myHours > 0){
+      process.stdout.write(myHours + ' godz.');
+    }
+    if ((myHours > 0)&&(myMinutes > 0)){
+      process.stdout.write(' ');
+    }
+    if (myMinutes > 0){
+      process.stdout.write(myMinutes + ' min');
+    }
+    if ((myHours > 0)&&(mySeconds > 0)||(myMinutes > 0)&&(mySeconds > 0)){
+      process.stdout.write(' ');
+    }
+    if (mySeconds > 0){
+      process.stdout.write(mySeconds + ' sek.\n');
+    }
+}
+exports.print = timeCalc;
