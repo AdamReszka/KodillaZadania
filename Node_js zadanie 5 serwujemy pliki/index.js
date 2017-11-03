13c26a194a2ee6c5d3a70c373fe4608746fa656d
AdamReszka <adam.reszka85@gmail.com>
2017-11-03 22:19:25 +0100

    load files

---
+++ Node_js zadanie 5 serwujemy pliki/index.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
+
+server.on('request', function (request, response){
+
+  if (request.method === 'GET' && request.url === '/'){
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    fs.readFile('./index.html','utf-8', function(err, data){
+      response.write(data);
+        response.end();
+    });
+  } else {
+    fs.readFile('./404.png','binary', function(err, file){
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.statusCode = 404;
+        response.write(file, "binary");
+        response.end();
+    });
+
+  }
+});
+server.listen(9000);
