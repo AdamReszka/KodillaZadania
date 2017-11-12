44d7061d48b3f6c95d30d22c2144098c704f87a5
AdamReszka <adam.reszka85@gmail.com>
2017-11-12 17:55:43 +0100

    add fourth react task

---
+++ React zadanie4 Liczniki/app/components/counter2.js
@@ -0,0 +1,60 @@
+var Counter2 = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 0
+    };
+  },
+  getDefaultProps:function(){
+    console.log('ustala ścieżkę do obrazka avatara, zanim użytkownik nie wgra swojego pliku');
+    return {source: '../images/defaultAvatar.jpg'}
+  },
+  increment: function() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+  decrement: function() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
+  reset: function() {
+    this.setState({
+      counter: this.state.counter = 0
+    });
+  },
+  shouldComponentUpdate: function(nextProps, nextState) {
+    console.log('DOM nie musi być aktualizowany gdy stan się nie zmienia. Drugie wciśnięcie resetu');
+    return nextState.counter !== this.state.counter;
+  },
+  componentWillUpdate: function() {
+    console.log('metoda może wyzwalać proces przekazania na serwer danych przekazanych przez użytkownika');
+  },
+  render: function() {
+    return React.createElement('div', {},
+      React.createElement('div', {className: 'counter-cont second-cont'},
+        React.createElement('div', {className: 'counter-display'},
+          React.createElement('span', {}, 'Middle counter ' + this.state.counter)
+        ),
+      ),
+      React.createElement('nav',{},
+        React.createElement('ul',{},
+          React.createElement('li', {className: 'my-button', onClick: this.increment},
+            React.createElement('p',{}, 'Dodaj')
+          ),
+          React.createElement('li', {className: 'my-button', onClick: this.decrement},
+            React.createElement('p',{}, 'Odejmij')
+          ),
+          React.createElement('li', {className: 'my-button', onClick: this.reset},
+            React.createElement('p',{}, 'Resetuj')
+          ),
+        ),
+
+      ),
+
+    );
+  },
+  componentDidUpdate: function() {
+    console.log('metoda może wywołać zmiany w widoku dla elementów, które powiązane są ze stanem i właściwiościami, które zostały zmienione');
+  },
+});
---
+++ React zadanie4 Liczniki/app/script.js
@@ -0,0 +1,29 @@
+var CounterCont = React.createClass({
+
+  componentWillMount: function() {
+    confirm('potwierdzam, że mam ukończone 18 lat');
+    console.log('pozwala zweryfikować spełnienie dodatkowych warunków zanim załaduje komponent');
+  },
+  componentDidMount: function() {
+    setTimeout(function(){
+      var newEmail = prompt('Zostaw email i zapisz się do newslettera');
+    }, 3000);
+    console.log('metoda może pozwalać na zebranie dodatkowych danych już po załadowaniu komponentu');
+  },
+  componentWillUnmount: function() {
+    console.log('odpięcie eventListenerów, gdyby to był moduł jakiegoś testu metoda mogłaby zliczać wyniki i przekazywać do komponentu zajmującego się wyświetlaniem oceny czy podsumowania');
+  },
+  render: function() {
+    return React.createElement('div',{className: 'counters-box'},
+      React.createElement(Counter),
+      React.createElement(Counter2),
+      React.createElement(Counter3)
+    );
+  },
+
+});
+
+
+var element = React.createElement(CounterCont);
+
+ReactDOM.render(element, document.getElementById('app'));
