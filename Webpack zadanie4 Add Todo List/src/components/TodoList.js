bfdad4d4e712e09e8ab81acac3a702b509ede77a
AdamReszka <adam.reszka85@gmail.com>
2017-11-28 19:07:55 +0100

    add list for todo app

--- Webpack zadanie4 Add Todo List/src/components/TodoList.js
+++ Webpack zadanie4 Add Todo List/src/components/TodoList.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import style from '../containers/App.css';
 
 class TodoList extends React.Component {
   constructor(props){
     super(props);
+    this.removeMe = this.removeMe.bind(this);
+  }
+  removeMe() {
+    console.log('it works');
   }
   render() {
+    const todos = this.props.todoList.map(item => <p onClick={this.removeMe} key={item.id}>{item.text}</p>);
     return (
-      const todos = props => <p>{props.todoList}</p>
+      <div className={style.todoItemsList}>
+        {todos}
+      </div>
     );
   }
 }
