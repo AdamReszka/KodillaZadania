6d87c237d40bc6b12a9b19398f8c69bd0ba76ea1
AdamReszka <adam.reszka85@gmail.com>
2017-11-29 01:08:31 +0100

    add task with env test

---
+++ Webpack zadanie5 node env test/src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import style from '../containers/App.css';
+import Todo from './Todo';
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    const todos = this.props.todoList.map((item, index) => {
+      return(
+        <div className={style.todoSingleItem}>
+          <Todo removeTodo={this.props.removeTodo} key={item.id} myIndex={index} item={item}/>
+        </div>
+
+      )
+    });
+    return (
+      <div className={style.todoItemsList}>
+        {todos}
+      </div>
+    );
+  }
+}
+
+export default TodoList;
