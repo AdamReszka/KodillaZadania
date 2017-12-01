5897612ac459c9e84c734d0d68da72cd4f93c420
AdamReszka <adam.reszka85@gmail.com>
2017-12-01 19:55:44 +0100

    add first task with express

---
+++ Express zadanie1 crud json/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+var stringifyFile;
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function (req, res) {
+  console.log('Otrzymałem żądanie GET do strony /getNote');
+  fs.readFile('./test.json', 'utf-8', function(err, data) {
+    if (err) throw err;
+    stringifyFile = data;
+    res.send(stringifyFile);
+  });
+
+
+});
+
+app.get('/updateNote/:note', function (req, res) {
+  stringifyFile += req.params.note;
+  fs.writeFile('./test.json', stringifyFile, function(err) {
+    if (err) throw err;
+    console.log('file update');
+  });
+  res.send('Dopisano' + req.params.note);
+});
+
+app.listen(3000);
