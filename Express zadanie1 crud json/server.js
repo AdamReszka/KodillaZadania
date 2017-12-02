df220c6621b1b70b30686e2d7a861112354db506
Adam Reszka <adam.reszka85@gmail.com>
2017-12-02 13:57:58 +0100

    fix http method

--- Express zadanie1 crud json/server.js
+++ Express zadanie1 crud json/server.js
@@ -2,10 +2,18 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var app = express();
-var stringifyFile;
+var stringifyFile = '';
 
 app.use(bodyParser.json());
 
+app.get('/test', function (req, res) {
+  res.send('trochę działa');
+});
+/*
+app.get('/test/:wpis', function (req, res) {
+  res.send('wpisałeś tu ' + req.params.wpis);
+})
+*/
 app.get('/getNote', function (req, res) {
   console.log('Otrzymałem żądanie GET do strony /getNote');
   fs.readFile('./test.json', 'utf-8', function(err, data) {
@@ -17,7 +25,7 @@ app.get('/getNote', function (req, res) {
 
 });
 
-app.get('/updateNote/:note', function (req, res) {
+app.post('/updateNote', function (req, res) {
   stringifyFile += req.params.note;
   fs.writeFile('./test.json', stringifyFile, function(err) {
     if (err) throw err;
@@ -25,5 +33,4 @@ app.get('/updateNote/:note', function (req, res) {
   });
   res.send('Dopisano' + req.params.note);
 });
-
 app.listen(3000);
