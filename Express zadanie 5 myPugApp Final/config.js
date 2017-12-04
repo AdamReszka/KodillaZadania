85b9ee558dad7d2259f9236851457eb43a662f3d
AdamReszka <adam.reszka85@gmail.com>
2017-12-04 21:59:29 +0100

    add final version of myPug App

---
+++ Express zadanie 5 myPugApp Final/config.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+module.exports = {
+  'GOOGLE_CLIENT_ID': '67012809449-rbp0ci9no8a89cv4u9sn6e0k8tkq52jq.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'lk0ghkxqDh2qk1iXNSFpg_qt',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
+exports default config;
