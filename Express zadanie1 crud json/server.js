e99a7e29b4c25eb5f709393259fcc9441fc1b228
AdamReszka <adam.reszka85@gmail.com>
2017-12-04 09:56:45 +0100

    fix code from first express task

--- Express zadanie1 crud json/server.js
+++ Express zadanie1 crud json/server.js
@@ -2,18 +2,10 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var app = express();
-var stringifyFile = '';
+var stringifyFile;
 
 app.use(bodyParser.json());
 
-app.get('/test', function (req, res) {
-  res.send('trochę działa');
-});
-/*
-app.get('/test/:wpis', function (req, res) {
-  res.send('wpisałeś tu ' + req.params.wpis);
-})
-*/
 app.get('/getNote', function (req, res) {
   console.log('Otrzymałem żądanie GET do strony /getNote');
   fs.readFile('./test.json', 'utf-8', function(err, data) {
@@ -25,7 +17,7 @@ app.get('/getNote', function (req, res) {
 
 });
 
-app.post('/updateNote', function (req, res) {
+app.get('/updateNote/:note', function (req, res) {
   stringifyFile += req.params.note;
   fs.writeFile('./test.json', stringifyFile, function(err) {
     if (err) throw err;
@@ -33,4 +25,5 @@ app.post('/updateNote', function (req, res) {
   });
   res.send('Dopisano' + req.params.note);
 });
+
 app.listen(3000);
